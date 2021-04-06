fb21406963418145b9769bed5e7265f508175630
Asad <asaduzzamannur.dev@gmail.com>
2021-04-07 02:05:02 +0600

    fixed minor indent issue on Assignment-08 plugin

--- assignment-08/asd-contact-ajax/templates/admin_menu_page.php
+++ assignment-08/asd-contact-ajax/templates/admin_menu_page.php
@@ -11,10 +11,10 @@
     <h1><?php esc_html_e( 'WeContact AJAX Form: Shortcode Documentation', 'asd-contact-ajax' ); ?></h1><br>
     <h2><?php esc_html_e( 'Form with title use guide', 'asd-contact-ajax' ); ?>:</h2>
     <h4><?php esc_html_e( 'Shortcode format', 'asd-contact-ajax' ); ?>:</h4>
-    <p><?php echo esc_html('[asd-sc-contact-form title="Title text" description="Description text"] [/asd-sc-contact-form]' ); ?></p>
+    <p><?php echo esc_html( '[asd-sc-contact-form title="Title text" description="Description text"] [/asd-sc-contact-form]' ); ?></p>
     <br>
     <h2><?php esc_html_e( 'Individual input field use guide', 'asd-contact-ajax' ); ?>:</h2>
     <h4><?php esc_html_e( 'Shortcode format', 'asd-contact-ajax' ); ?>:</h4>
-    <p><?php echo esc_html('[asd-sc-contact-field type="field type" name="field name" label="Field label text" placehoder="Placeholder Text" value="Default value"]' ); ?></p>
+    <p><?php echo esc_html( '[asd-sc-contact-field type="field type" name="field name" label="Field label text" placehoder="Placeholder Text" value="Default value"]' ); ?></p>
 </div>
 <?php
